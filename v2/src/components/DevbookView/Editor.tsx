75c66032639124cfd8a32e5318fc91820aa7f04a
Tomas Valenta <valenta.and.thomas@gmail.com>
2022-01-21 14:43:34 +0000

    Add dDevbook integration [WIP]

---
+++ v2/src/components/DevbookView/Editor.tsx
@@ -0,0 +1,71 @@
+import React, {
+  useEffect,
+  useRef,
+  memo,
+} from 'react';
+import { EditorState, EditorView, basicSetup } from '@codemirror/basic-setup';
+import {
+  keymap,
+} from '@codemirror/view'
+import {
+  defaultKeymap,
+  indentWithTab,
+} from '@codemirror/commands'
+import {
+  javascriptLanguage,
+} from '@codemirror/lang-javascript'
+
+import './Editor.css';
+
+export interface Props {
+  initialCode: string
+  onChange: (content: string) => void
+}
+
+function Editor({
+  initialCode,
+  onChange,
+}: Props) {
+  const editorEl = useRef<HTMLDivElement>(null);
+
+  useEffect(function createEditor() {
+    if (!editorEl.current) return;
+
+    const changeWatcher = EditorView.updateListener.of(update => {
+      if (update.docChanged) {
+        onChange(update.state.doc.toString());
+      }
+    });
+    const state = EditorState.create({
+      doc: initialCode,
+      extensions: [
+        basicSetup,
+        changeWatcher,
+        javascriptLanguage,
+        keymap.of([
+          ...defaultKeymap,
+          indentWithTab,
+          // Override default browser Ctrl/Cmd+S shortcut when a code cell is focused.
+          {
+            key: 'Mod-s',
+            run: () => true,
+          },
+        ]),
+      ],
+    });
+
+    const view = new EditorView({ state, parent: editorEl.current });
+    return () => {
+      view.destroy();
+    };
+  }, [initialCode, onChange, editorEl]);
+
+  return (
+    <div
+      className="editor"
+      ref={editorEl}
+    />
+  )
+}
+
+export default memo(Editor);
